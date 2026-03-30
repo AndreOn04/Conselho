aec36856d84ade6cad517b12eefda2ba2146f601
Marcio Andre <andreonlinemt@yahoo.com>
2026-03-30 18:44:44 -0400

    Novas functions

--- src/js/Formspree.js
+++ src/js/Formspree.js
@@ -5,66 +5,65 @@ document.addEventListener("DOMContentLoaded", () => {
   const btnConfirm = document.getElementById("btn-confirm");
 
   if (contactForm && modal) {
-    
     contactForm.addEventListener("submit", (e) => {
       e.preventDefault();
       modal.classList.add("on-visible");
     });
 
     if (btnEdit) {
-        btnEdit.addEventListener("click", () => {
-          modal.classList.remove("on-visible");
-        });
+      btnEdit.addEventListener("click", () => {
+        modal.classList.remove("on-visible");
+      });
     }
 
     if (btnConfirm) {
-        btnConfirm.addEventListener("click", async () => {
-          modal.classList.remove("on-visible");
-    
-          const btnSubmit = contactForm.querySelector(".btn-send");
-          const originalText = btnSubmit.innerHTML;
-    
-          btnSubmit.disabled = true;
-          btnSubmit.style.cursor = "wait";
-    
-          let timeLeft = 30;
-          const countdown = setInterval(() => {
-            btnSubmit.innerHTML = `<ion-icon name="sync-outline" class="spinning"></ion-icon> Processando (${timeLeft}s)`;
-            timeLeft--;
-    
-            if (timeLeft < 0) {
-              clearInterval(countdown);
-            }
-          }, 1000);
-    
-          await new Promise((resolve) => setTimeout(resolve, 3000));
-    
-          const formData = new FormData(contactForm);
-    
-          try {
-            btnSubmit.innerHTML = `<ion-icon name="cloud-upload-outline" class="spinning"></ion-icon> Finalizando...`;
-    
-            const response = await fetch("https://formspree.io/f/xaqpppjv", {
-              method: "POST",
-              body: formData,
-              headers: { Accept: "application/json" },
-            });
-    
-            if (response.ok) {
-              clearInterval(countdown); 
-              contactForm.reset();
-              window.location.href = "https://formspree.io/thanks?language=pt";
-            } else {
-              throw new Error("Erro no envio");
-            }
-          } catch (error) {
+      btnConfirm.addEventListener("click", async () => {
+        modal.classList.remove("on-visible");
+
+        const btnSubmit = contactForm.querySelector(".btn-send");
+        const originalText = btnSubmit.innerHTML;
+
+        btnSubmit.disabled = true;
+        btnSubmit.style.cursor = "wait";
+
+        let timeLeft = 30;
+        const countdown = setInterval(() => {
+          btnSubmit.innerHTML = `<ion-icon name="sync-outline" class="spinning"></ion-icon> Processando (${timeLeft}s)`;
+          timeLeft--;
+
+          if (timeLeft < 0) {
             clearInterval(countdown);
-            alert("Ocorreu um erro ao enviar. Por favor, tente novamente.");
-            btnSubmit.disabled = false;
-            btnSubmit.innerHTML = originalText;
-            btnSubmit.style.cursor = "pointer";
           }
-        });
+        }, 1000);
+
+        await new Promise((resolve) => setTimeout(resolve, 3000));
+
+        const formData = new FormData(contactForm);
+
+        try {
+          btnSubmit.innerHTML = `<ion-icon name="cloud-upload-outline" class="spinning"></ion-icon> Finalizando...`;
+
+          const response = await fetch("https://formspree.io/f/xaqpppjv", {
+            method: "POST",
+            body: formData,
+            headers: { Accept: "application/json" },
+          });
+
+          if (response.ok) {
+            clearInterval(countdown);
+            contactForm.reset();
+            window.location.href = "https://formspree.io/thanks?language=pt";
+          } else {
+            throw new Error("Erro no envio");
+          }
+        } catch (error) {
+          clearInterval(countdown);
+          alert("Ocorreu um erro ao enviar. Por favor, tente novamente.");
+          btnSubmit.disabled = false;
+          btnSubmit.innerHTML = originalText;
+          btnSubmit.style.cursor = "pointer";
+        }
+      });
     }
   }
 });
